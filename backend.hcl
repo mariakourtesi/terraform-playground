b0813729ea34faf4a5116ce9b8de6281749b0180
Maria <mkourtesi@hotmail.com>
2021-06-17 19:32:14 +0100

    feat: adding db

--- backend.hcl
+++
@@ -1,4 +0,0 @@
- bucket = "terraform-up-and-running-state_marias_bucket"
- region = "us-east-2"
- dynamodb_table = "terraform-up-and-running"
- encrypt = true
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -7,11 +7,7 @@ resource "aws_launch_configuration" "example" {
     instance_type = "t2.micro"
     security_groups = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+    user_data = data.template_file.user_data.rendered
 
 # Required when using a launch configuration with an auto scaling group 
 lifecycle {
@@ -139,5 +135,15 @@ data "aws_subnet_ids" "default" {
 terraform {
     backend "s3" {
         key = "stage/services/webserver-cluster/terraform.tfstate"
+    }
+}
+
+data "template_file" "user_data" {
+    template = file("user-data.sh")
+
+    vars = {
+        server_port = var.server_port
+        db_address = data.terraform_remote_state.db.outputs.address
+        db_port = data.terraform_remote_state.db.outputs.port
     }
 }
