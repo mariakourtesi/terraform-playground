d02e58df0f4465ed4840d8f162ae890bc45f122e
Maria <mkourtesi@hotmail.com>
2021-06-19 10:41:26 +0100

    feat: add separate resources for alb security group

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -84,24 +84,29 @@ resource "aws_lb_listener_rule" "asg" {
 
 resource "aws_security_group" "alb"{
         name = "${var.cluster_name}-alb"
+}
 
-        # Allow all inbound requests
-        ingress {
+resource "aws_security_group_rule" "allow_http_inbound" {
+      type = "ingress"
+      security_group_id = aws_security_group.alb.id
+      # Allow all inbound requests
             from_port   = local.http_port
             to_port     = local.http_port
             protocol    = local.tcp_protocol
             cidr_blocks = local.all_ips
-        }
+}
 
+resource "aws_security_group_rule" "allow_all_outbound" {
+      type = "egress"
+      security_group_id = aws_security_group.alb.id
+  
         # Allow all outbound requests
-        egress {
             from_port   = local.any_port
             to_port     = local.any_port
             protocol    = local.any_protocol
             cidr_blocks = local.all_ips
-
-        }
 }
+
 
 resource "aws_lb_target_group" "asg" {
     name = "terraform-asg-example"
@@ -161,3 +166,4 @@ locals {
     tcp_protocol = "tcp"
     all_ips = ["0.0.0.0/0"]
 }
+
--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -12,3 +12,8 @@ output "alb_dns_name" {
     value = aws_lb.example.dns_name
     description = "The domain name of the load balancer"
 }
+
+output "alb_security_group" {
+    value = aws_security_group.alb.id
+    description = "The ID of the Security Group attached to the load balancer."
+}
