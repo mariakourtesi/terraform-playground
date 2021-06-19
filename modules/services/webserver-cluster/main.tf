1dfa8761a90534cfe6c50f44b224993c83fadc30
Maria <mkourtesi@hotmail.com>
2021-06-19 08:30:40 +0100

    feat: add a local values to module

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -18,10 +18,10 @@ resource "aws_security_group" "instance" {
     name = "${var.cluster_name}-instance"
 
     ingress {
-        from_port = var.server_port
-        to_port =  var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = local.server_port
+        to_port =  local.server_port
+        protocol = local.tcp_protocol
+        cidr_blocks = local.all_ips
     }
 }
 
@@ -51,7 +51,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
     load_balancer_arn = aws_lb.example.arn
-    port = 80
+    port = local.http_port
     protocol = "HTTP"
     
     # By default return a simple 404 page
@@ -87,18 +87,18 @@ resource "aws_security_group" "alb"{
 
         # Allow all inbound requests
         ingress {
-            from_port   = 80
-            to_port     = 80
-            protocol    = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
+            from_port   = local.http_port
+            to_port     = local.http_port
+            protocol    = local.tcp_protocol
+            cidr_blocks = local.all_ips
         }
 
         # Allow all outbound requests
         egress {
-            from_port   = 0
-            to_port     = 0
-            protocol    = "-1"
-            cidr_blocks = ["0.0.0.0/0"]
+            from_port   = local.any_port
+            to_port     = local.any_port
+            protocol    = local.any_protocol
+            cidr_blocks = local.all_ips
 
         }
 }
@@ -152,4 +152,12 @@ data "terraform_remote_state" "db" {
       key = var.db_remote_state_key
       region = "us-east-2"
   }
+}
+
+locals {
+    http_port = 80
+    any_port = 0
+    any_protocol = "-1"
+    tcp_protocol = "tcp"
+    all_ips = ["0.0.0.0/0"]
 }
